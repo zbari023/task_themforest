572b3fa278e74ede6e246e11b1267742829e03ac
zbari023 <barry450643@gmail.com>
2023-07-02 22:35:47 +0200

    add work

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sochial',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
--- sochial/admin.py
+++ sochial/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 from .models import Post , Comment
 
 
 
 
-class PostAdmin(admin.ModelAdmin):
+
+
+class PostAdmin(SummernoteModelAdmin):
     list_display = ['user', 'create_date']
     list_filter = ['user', 'create_date']
     search_fields = ['user']
+    summernote_fields = '__all__'
     
 
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post, PostAdmin )
 admin.site.register(Comment)
